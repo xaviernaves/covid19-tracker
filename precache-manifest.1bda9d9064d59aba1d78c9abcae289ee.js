d9b78791ee8b19528005906478201f6f36af2d5d
Xavier Naves <xavinaves@gmail.com>
2020-12-31 03:00:36 +0100

    initial commit

---
+++ precache-manifest.1bda9d9064d59aba1d78c9abcae289ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477cc11c1be4020105ef138e51526ef3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e48ae71ce04084c498b9",
+    "url": "./static/css/main.b4288b23.chunk.css"
+  },
+  {
+    "revision": "e9c6edb34349defd8b8a",
+    "url": "./static/js/2.aff44e39.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "./static/js/2.aff44e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48ae71ce04084c498b9",
+    "url": "./static/js/main.f34cb698.chunk.js"
+  },
+  {
+    "revision": "20d5cc9b8d49ef698a9a",
+    "url": "./static/js/runtime-main.9843b6cc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "./static/media/image.d7265326.png"
+  }
+]);
